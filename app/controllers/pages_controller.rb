292302fbf536b2450c2edac94c403f7fd5dd09cc
Stephan Yu <Stephan.yu@gmail.com>
2015-06-04 13:07:21 +0200

    added user authentication functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def index
+    redirect_to home_path if current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Myflix::Application.routes.draw do
   root to: 'pages#index'
   get 'register', to: 'users#new'
-  get 'signin', to: 'sessions#new', as: :sign_in
+  get 'sign_in', to: 'sessions#new'
+  get 'sign_out', to: 'sessions#destroy'
 
   resources :users, only:[:create]
+  resources :sessions, only:[:create]
 
   get 'home', to: 'categories#index'
   resources :categories, only: [:show]
